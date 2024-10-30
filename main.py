fdfce32252d028a57b1cb59e2afab0b8cf27ee57
Luis <luis.lacoste@hotmail.com>
2024-10-30 19:43:13 -0300

    attempting to generate sudoku

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
 
-# facil <= 7
-# medio = a 5
 # Tamaño del tablero
 TAB_N = 9
 # Tamaño de cada subcuadro (2x4 en este caso)
@@ -15,6 +13,10 @@ def print_board(tabla):
 
 
 def is_safe(tabla, row, col, num):
+    # Verifica si la celda ya está ocupada
+    if tabla[row][col] != 0:
+        return False
+
     # Verificar fila
     if num in tabla[row]:
         return False
@@ -46,13 +48,16 @@ def solve_sudoku(tabla):
                             return True
                         # Deshace la asignación (backtrack)
                         tabla[row][col] = 0
+                # Si no se puede colocar ningún número, imprime la posición
+                print(f"No se puede resolver en la posición ({row}, {col})")
                 return False
     return True
 
 
-def generar_random(tabla):
+
+def generar_random(tabla, celdas):
     # Llenar algunas celdas al azar para iniciar la resolución
-    for _ in range(random.randint(16, 32)):  # Rellenar entre 16 y 32 celdas
+    for _ in range(celdas):
         row = random.randint(0, TAB_N - 1)
         col = random.randint(0, TAB_N - 1)
         num = random.randint(1, TAB_N)
@@ -64,66 +69,46 @@ def generar_random(tabla):
     return tabla
 
 
-def generar_custom(tabla):
-    # dejamos al usuario gelejir 10 celdas a rellenar aleatoriamente
-    for _ in range(10):
-        row = random.randint(0, TAB_N - 1)
-        col = random.randint(0, TAB_N - 1)
-        num = int(input("Ingrese el número: "))
-        while num <= 1 or num > 9:
-            num = int(input("Ingrese el número: "))
+def generar_tablero(dificultad):
+  tabla = [[0] * TAB_N for _ in range(TAB_N)]
+  if dificultad == 1:
+      celdas = random.randint(35, 50)
+  elif dificultad == 2:
+      celdas = random.randint(22, 34)
+  elif dificultad == 3:
+      celdas = random.randint(10, 21)
+  
+  
+  
+  # llena x cantidad de celdas
+  tabla = generar_random(tabla, celdas)
 
-        tabla[row][col] = num
-
-    return tabla
-
-
-def generate_sudoku(generar_metodo):
-    tabla = [[0] * TAB_N for _ in range(TAB_N)]
-
-    if generar_metodo == 1:
-
-        # Generar un tablero de Sudoku
-        tabla = generar_random(tabla)
-    elif generar_metodo == 2:
-        # Generar un tablero de Sudoku
-        tabla = generar_custom(tabla)
-
-    return tabla
-
-
-def generar_tablero():
-    return [[0] * TAB_N for _ in range(TAB_N)]
-
+  print(tabla)
+  return tabla
 
 
 def main():
+    # @TODO arreglar esta logica de mierda de validacion
+    dificultad = 1
+    # while dificultad != 1 and dificultad != 2 and dificultad != 3:
+    #     dificultad = int(
+    #         input("Ingrese la dificultad del sudoku \n 1: Facil\n 2: Medio\n 3: Dificil \n "))
 
-    ## @TODO arreglar esta logica de mierda de validacion
-    dificultad = 0
-    while dificultad != 1 and dificultad != 2 and dificultad != 3:
-        dificultad = int(
-            input("Ingrese la dificultad del sudoku \n 1: Facil\n 2: Medio\n 3: Dificil: "))
-        
-    if dificultad == 1:
-        tablero = generar_tablero()
-    elif dificultad == 2:
-        tablero = generar_tablero()
-    elif dificultad == 3:
-        tablero = generar_tablero()
+    tablero = generar_tablero(dificultad)
 
-    generar_metodo = 0
-    while generar_metodo != 1 and generar_metodo != 2:
-        generar_metodo = int(input(
-            "Ingrese el método de generación de tablero de Sudoku (1: Aleatorio, 2: Personalizado): "))
+    
+    # generar_metodo = 0
+    # while generar_metodo != 1 and generar_metodo != 2:
+    #     generar_metodo = int(input(
+    #         "Ingrese el método de generación de tablero de Sudoku (1: Aleatorio, 2: Personalizado): "))
 
-    sudoku_board = generate_sudoku(generar_metodo)
+    # sudoku_board = generate_sudoku(generar_metodo)
     print("Tablero de Sudoku inicial:")
-    print_board(sudoku_board)
+    print_board(tablero)
 
-    if solve_sudoku(sudoku_board):
+    if solve_sudoku(tablero):
         print("\nTablero de Sudoku resuelto:")
-        print_board(sudoku_board)
+        print_board(tablero)
     else:
         print("No se pudo resolver el Sudoku.")
 
